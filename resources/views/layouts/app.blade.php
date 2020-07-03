e8b735bb154e1d4a4e72aaa1758ee5277ed7e138
masatoshi.tezuka <masted3325@gmail.com>
2020-07-03 11:58:54 +0000

    6th valitation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,13 @@
         </header>
 
         <div class="container">
+            {{--エラーメッセージ--}}
+            @include("commons.error_tasks")
+            
             @yield('content')
         </div>
-
+        
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
